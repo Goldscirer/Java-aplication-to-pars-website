fd1bba55211e831324dcf2629006e217b781b078
Seweryn Wawrzynowicz <seweryn.wawrzynowicz@globallogic.com>
2019-10-16 10:18:14 +0200

    Master: Prepare list of teams

--- src/main/java/RegExr/Main.java
+++ src/main/java/RegExr/Main.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+
+    private static final Pattern TAG_REGEX = Pattern.compile("<td class=\"druzyna\">(.+?)</td>", Pattern.DOTALL);
+
     public static void main(String[] args) throws IOException {
         String content = null;
         URLConnection connection = null;
@@ -25,13 +31,26 @@ public class Main {
         try (PrintWriter out = new PrintWriter("index.html")) {
             out.println(content);
         }
-        System.out.println(content);
-        final Pattern pattern = Pattern.compile("html\">(.+?)</a>", Pattern.DOTALL);
+
+        System.out.println(Arrays.toString(getTagValues(content).toArray()));
+
+        final Pattern pattern = Pattern.compile("<td class=\"druzyna\">(.+?)</td>", Pattern.DOTALL);
         final Matcher matcher = pattern.matcher(content);
         matcher.find();
-        System.out.println(matcher.group(1));
+        final Pattern longTeamString = Pattern.compile(">(.+?)<", Pattern.DOTALL);
+        final Matcher teamMatcher = longTeamString.matcher(matcher.group(1));
+        teamMatcher.find();
+    }
 
-
-        //System.out.println("TO jest moja zajebista srona" + content);
+    private static List<String> getTagValues(final String str) {
+        final List<String> tagValues = new ArrayList<String>();
+        final Matcher matcher = TAG_REGEX.matcher(str);
+        while (matcher.find()) {
+            final Pattern longTeamString = Pattern.compile(">(.+?)<", Pattern.DOTALL);
+            final Matcher teamMatcher = longTeamString.matcher(matcher.group(1));
+            teamMatcher.find();
+            tagValues.add(teamMatcher.group(1));
+        }
+        return tagValues;
     }
 }
