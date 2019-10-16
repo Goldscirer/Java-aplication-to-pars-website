913a8257fa03f3e3631f0e836d176ac276b0c0a5
Seweryn <seweryn.wawrzynowicz@gmail.com>
2019-10-16 09:00:25 +0200

    Try set regex

--- src/main/java/RegExr/Main.java
+++ src/main/java/RegExr/Main.java
@@ -1,16 +1,19 @@
 package RegExr;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String content = null;
         URLConnection connection = null;
         try {
-            connection =  new URL("https://www.plk.pl/terminarz-i-wyniki.html").openConnection();
+            connection =  new URL("https://www.plk.pl/tabele.html").openConnection();
             Scanner scanner = new Scanner(connection.getInputStream());
             scanner.useDelimiter("\\Z");
             content = scanner.next();
@@ -18,7 +21,17 @@ public class Main {
         }catch ( Exception ex ) {
             ex.printStackTrace();
         }
+
+        try (PrintWriter out = new PrintWriter("index.html")) {
+            out.println(content);
+        }
         System.out.println(content);
-        System.out.println("TO jest moja zajebista srona" + content);
+        final Pattern pattern = Pattern.compile("html\">(.+?)</a>", Pattern.DOTALL);
+        final Matcher matcher = pattern.matcher(content);
+        matcher.find();
+        System.out.println(matcher.group(1));
+
+
+        //System.out.println("TO jest moja zajebista srona" + content);
     }
 }
